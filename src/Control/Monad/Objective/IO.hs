9785c720625d5948b73a093374c21cc2a1bea4ca
Fumiaki Kinoshita <fumiexcel@gmail.com>
2014-11-13 14:46:54 +0900

    unbind invocation

--- src/Control/Monad/Objective/IO.hs
+++ src/Control/Monad/Objective/IO.hs
@@ -20,12 +20,11 @@ import Control.Object
 
 instance MonadObjective IO where
 
-  type Residence IO = IO
-  data Address e IO = Address (MVar (Object e IO))
+  data Address e m IO = Address (MVar (Object e m))
 
-  Address m .- e = do
-      c <- takeMVar m
+  Address m `invoke` e = do
+    c <- takeMVar m
+    return $ do
       (a, c') <- runObject c e
-      putMVar m c'
-      return a
+      return (putMVar m c' >> return a)
   new v = Address `fmap` newMVar v
