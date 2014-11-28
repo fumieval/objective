cf6b7ec7525642838d33a20cbfc2dd0149457cf2
Fumiaki Kinoshita <fumiexcel@gmail.com>
2014-11-28 17:59:48 +0900

    (.&)

--- src/Control/Monad/Objective/Class.hs
+++ src/Control/Monad/Objective/Class.hs
@@ -23,10 +23,11 @@ import Control.Object
 
 import Control.Monad
 import Control.Elevator
+import Control.Monad.Trans.State.Strict
 
 type Instance' e m = Instance e m m
 
-class Monad m => MonadObjective m where
+class (Tower m, Monad m) => MonadObjective m where
   data Instance (e :: * -> *) (n :: * -> *) m
   -- | Send a message to the pointed one.
   invoke :: Monad n => Instance e n m -> e a -> m (n (m a))
@@ -39,6 +40,7 @@ class Monad m => MonadObjective m where
   , MonadObjective m
   , Elevate m f) => Instance e n m -> e a -> f a
 a .- e = elevate $ invoke a e >>= join . elevate
+{-# INLINE (.-) #-}
 
 infix 3 .-
 
@@ -49,5 +51,13 @@ infix 3 .-
   , MonadObjective m
   , Elevate m g) => Instance f n m -> e a -> g a
 a .^ e = a .- elevate e
+{-# INLINE (.^) #-}
 
 infix 3 .^
+
+-- | Specialized (.^) for StateT
+(.&) :: (MonadObjective m, Elevate (StateT s m) e, Monad n, Elevate n m, Elevate m f) => Instance e n m -> StateT s m a -> f a
+(.&) = (.^)
+{-# INLINE (.&) #-}
+infix 3 .&
+
